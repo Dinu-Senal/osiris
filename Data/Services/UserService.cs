3691133e6f71f207c797e2731b7329fd16896ce9
Dinu-Senal <60167570+Dinu-Senal@users.noreply.github.com>
2021-11-27 10:12:03 +0530

    User Service

---
+++ Data/Services/UserService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Osiris.Data.Services
+{
+    public class UserService
+    {
+        #region Property
+        #endregion
+
+        #region Property
+        private readonly ApplicationDbContext _applicationDbContext;
+        #endregion
+
+        #region Constructor
+        public UserService(ApplicationDbContext appDbContext)
+        {
+            _applicationDbContext = appDbContext;
+        }
+        #endregion
+
+        #region Get All Users
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _applicationDbContext.User.ToListAsync();
+        }
+        #endregion
+
+        #region Add User
+        public async Task<bool> AddUserAsync(User user)
+        {
+            await _applicationDbContext.User.AddAsync(user);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Get User by Id
+        public async Task<User> GetUserAsync(Guid Id)
+        {
+            User user = await _applicationDbContext.User.FirstOrDefaultAsync(data => data.UserId.Equals(Id));
+            return user;
+        }
+        #endregion
+
+        #region Update User
+        public async Task<bool> UpdateUserAsync(User user)
+        {
+            _applicationDbContext.User.Update(user);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Delete User
+        public async Task<bool> DeleteUserAsync(User user)
+        {
+            _applicationDbContext.User.Remove(user);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+    }
+}
